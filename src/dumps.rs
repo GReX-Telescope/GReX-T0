c057361a0b4f291056dcc5e79e955a307b34c969
Kiran Shila <me@kiranshila.com>
2024-03-18 13:32:02 -0700

    use udp data on voltage trigger for filename

--- src/dumps.rs
+++ src/dumps.rs
@@ -39,10 +39,7 @@ impl DumpRing {
     }
 
     // Pack the ring into an array of [time, (pol_a, pol_b), channel, (re, im)]
-    pub fn dump(&self, start_time: &Epoch, path: &Path) -> eyre::Result<()> {
-        // Filename with ISO 8610 standard format
-        let fmt = Format::from_str("%Y%m%dT%H%M%S").unwrap();
-        let filename = format!("grex_dump-{}.nc", Formatter::new(Epoch::now()?, fmt));
+    pub fn dump(&self, start_time: &Epoch, path: &Path, filename: &str) -> eyre::Result<()> {
         let file_path = path.join(filename);
         let mut file = netcdf::create(file_path)?;
 
@@ -110,7 +107,7 @@ impl DumpRing {
 }
 
 pub async fn trigger_task(
-    sender: Sender<()>,
+    sender: Sender<Vec<u8>>,
     port: u16,
     mut shutdown: broadcast::Receiver<()>,
 ) -> eyre::Result<()> {
@@ -118,16 +115,18 @@ pub async fn trigger_task(
     // Create the socket
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let sock = UdpSocket::bind(addr).await?;
-    // Maybe even 0 would work, we don't expect data
-    let mut buf = [0; 10];
+    // We expect something like a UUIDv4 string, which is 36 characters, round up to 64 seems fine
+    let mut buf = [0; 64];
     loop {
         tokio::select! {
             _ = shutdown.recv() => {
                 info!("Voltage ringbuffer trigger task stopping");
                 break;
             }
+            // Receive bytes from the socket, optionally containing a file suffix
+            // And send to the dump task
             _ = sock.recv_from(&mut buf) => {
-                sender.send(())?;
+                sender.send(buf.to_vec())?;
             }
         }
     }
@@ -137,21 +136,39 @@ pub async fn trigger_task(
 pub fn dump_task(
     mut ring: DumpRing,
     payload_reciever: StaticReceiver<Payload>,
-    signal_reciever: Receiver<()>,
+    signal_reciever: Receiver<Vec<u8>>,
     start_time: Epoch,
     path: PathBuf,
     mut shutdown: broadcast::Receiver<()>,
 ) -> eyre::Result<()> {
     info!("Starting voltage ringbuffer fill task!");
+    // Create timestamp format for fallback filename
+    // Filename with ISO 8610 standard format
+    let fmt = Format::from_str("%Y%m%dT%H%M%S").unwrap();
     loop {
         if shutdown.try_recv().is_ok() {
             info!("Dump task stopping");
             break;
         }
         // First check if we need to dump, as that takes priority
-        if signal_reciever.try_recv().is_ok() {
-            info!("Dumping ringbuffer");
-            match ring.dump(&start_time, &path) {
+        if let Ok(bytes) = signal_reciever.try_recv() {
+            let mut filename_suffix = match String::from_utf8(bytes) {
+                Ok(s) => s,
+                Err(_) => {
+                    warn!(
+                        "Incoming voltage dump trigger filename invalid, falling back to timestamp"
+                    );
+                    format!("{}", Formatter::new(Epoch::now()?, fmt))
+                }
+            };
+            // Don't write whitespace
+            if filename_suffix.is_empty() || filename_suffix.chars().all(|c| c.is_whitespace()) {
+                warn!("Incoming voltage dump trigger filename was empty or all whitespace, falling back to timestamp");
+                filename_suffix = format!("{}", Formatter::new(Epoch::now()?, fmt));
+            }
+            let filename = format!("grex_dump-{}.nc", filename_suffix);
+            info!("Dumping ringbuffer to file: {}", filename);
+            match ring.dump(&start_time, &path, &filename) {
                 Ok(_) => (),
                 Err(e) => warn!("Error in dumping buffer - {}", e),
             }
