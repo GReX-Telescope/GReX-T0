7bfc03641c7e6704850581ee04058a0466315480
Kiran Shila <me@kiranshila.com>
2024-05-31 16:48:25 -0700

    Back to real injection

--- src/injection.rs
+++ src/injection.rs
@@ -61,9 +61,10 @@ impl Injections {
 pub fn inject(pl: &mut Payload, sample: &[i8]) {
     // For both polarizations, add the real part by the value of the corresponding channel in the fake pulse data
     for (i, &sample) in sample.iter().enumerate() {
+        // Don't bother adding zeros
         if sample != 0 {
-            pl.pol_a[i].0.re = 127;
-            pl.pol_b[i].0.re = 127;
+            pl.pol_a[i].0.re += sample;
+            pl.pol_b[i].0.re += sample;
         }
     }
 }
