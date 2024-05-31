557417d0d6efd933633fa4c81354b285ab8976ec
Kiran Shila <me@kiranshila.com>
2024-05-31 16:17:58 -0700

    max non zeros

--- src/injection.rs
+++ src/injection.rs
@@ -61,8 +61,8 @@ impl Injections {
 pub fn inject(pl: &mut Payload, sample: &[i8]) {
     // For both polarizations, add the real part by the value of the corresponding channel in the fake pulse data
     for (i, sample) in sample.iter().enumerate() {
-        pl.pol_a[i].0.re += *sample;
-        pl.pol_b[i].0.re += *sample;
+        pl.pol_a[i].0.re += 127 * *sample;
+        pl.pol_b[i].0.re += 127 * *sample;
     }
 }
 
