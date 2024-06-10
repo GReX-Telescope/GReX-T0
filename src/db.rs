54633666193966e1844f31ca4b5a86e30de8adc7
Kiran Shila <me@kiranshila.com>
2024-06-10 12:42:39 -0700

    SQL typo

--- src/db.rs
+++ src/db.rs
@@ -2,18 +2,23 @@
 use rusqlite::{Connection, Result};
 use std::path::PathBuf;
 
-/// Connect to the database, and create the injection table if it doesn't already exist
-pub fn connect_and_create(db_path: PathBuf) -> Result<Connection> {
-    let conn = Connection::open(db_path)?;
+fn create_table(conn: &Connection) -> Result<()> {
     conn.execute(
         "CREATE TABLE IF NOT EXISTS injection (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         mjd REAL NOT NULL,
         filename TEXT NOT NULL,
         sample INTEGER NOT NULL
-    ) STRICT;",
+    ) STRICT",
         (),
     )?;
+    Ok(())
+}
+
+/// Connect to the database, and create the injection table if it doesn't already exist
+pub fn connect_and_create(db_path: PathBuf) -> Result<Connection> {
+    let conn = Connection::open(db_path)?;
+    create_table(&conn)?;
     Ok(conn)
 }
 
@@ -28,9 +33,26 @@ impl InjectionRecord {
     /// Insert an injection record into the connected database
     pub fn db_insert(&self, conn: &Connection) -> Result<()> {
         conn.execute(
-            "INSERT INTO injection (mjd, filename, sample)",
+            "INSERT INTO injection (mjd, filename, sample) VALUES (?1, ?2, ?3)",
             (&self.mjd, &self.filename, &self.sample),
         )?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_db() {
+        let conn = Connection::open_in_memory().unwrap();
+        create_table(&conn).unwrap();
+        let ir = InjectionRecord {
+            mjd: 123.456,
+            filename: "foo".to_owned(),
+            sample: 12345,
+        };
+        ir.db_insert(&conn).unwrap()
     }
 }
